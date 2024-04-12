769b0dba3ebde991c30b72acda3df787aa126d41
htunyarzar80 <htunyarzar2001@gmail.com>
2024-04-12 13:57:12 +0630

    [Complete] Ado.Net CRUD

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -39,10 +39,10 @@ Console.WriteLine("Hello, World!");
 
 //}
 AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
-//adoDotNetExample.Read();
-//adoDotNetExample.Create("title","author","content");
-//adoDotNetExample.Update(12, "test title", "test author", "test content");
-//adoDotNetExample.Delete(11);
+adoDotNetExample.Read();
+adoDotNetExample.Create("title", "author", "content");
+adoDotNetExample.Update(12, "test title", "test author", "test content");
+adoDotNetExample.Delete(11);
 adoDotNetExample.Edit(1); 
 
 Console.ReadKey();
