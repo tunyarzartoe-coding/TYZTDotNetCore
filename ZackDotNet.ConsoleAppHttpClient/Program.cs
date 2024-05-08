55bec7d3717fcf61bb55acfc08904e1db9aa9a56
htunyarzar80 <htunyarzar2001@gmail.com>
2024-05-09 00:55:46 +0630

    [Complete] ConsoleAppHttpClient

---
+++ ZackDotNet.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string jsonstr = await File.ReadAllTextAsync("data.json");
+var model = JsonConvert.DeserializeObject<MainDto>(jsonstr);
+
+Console.WriteLine(jsonstr);
+
+foreach (var question in model.questions)
+{
+    Console.WriteLine(question.questionNo);
+}
+
+//JSON to C## Need Package 
+//C## to JSON
+
+Console.ReadLine();
+
+static string ToNumber (string num)
+{
+    num.Replace ('၃', '3');
+    return num;
+}
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
