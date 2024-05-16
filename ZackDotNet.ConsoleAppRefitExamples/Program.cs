b2f5f41f90f64456bafe951ef27c5277b5fcca9f
htunyarzar80 <htunyarzar2001@gmail.com>
2024-05-16 14:16:00 +0630

    [Complete] ConsoleAppRefitExamples CRUD

---
+++ ZackDotNet.ConsoleAppRefitExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ZackDotNet.ConsoleAppRefitExamples;
+
+//try
+//{
+//    RefitExample refitExample = new RefitExample();
+//    await refitExample.RunAsync();
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.ToString());
+//}
+
+RefitExample refitExample = new RefitExample();
+await refitExample.RunAsync();
