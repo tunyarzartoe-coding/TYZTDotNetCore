eb31e36957523acc23a56f48532b4171f32d12f9
htunyarzar80 <htunyarzar2001@gmail.com>
2024-05-17 22:10:05 +0630

    [Complete] WinForms Create

---
+++ ZackDotNet.WinFormsApp/Form1.cs
@@ -0,0 +1,15 @@
+namespace ZackDotNet.WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World.");
+        }
+    }
+}
