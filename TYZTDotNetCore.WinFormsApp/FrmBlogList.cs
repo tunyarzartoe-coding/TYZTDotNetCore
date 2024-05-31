0298fd9c79867b00e62bbb1051aeda704921f717
htunyarzar80 <htunyarzar2001@gmail.com>
2024-05-31 13:56:43 +0630

    [Complete] Switch Case

--- TYZTDotNetCore.WinFormsApp/FrmBlogList.cs
+++ TYZTDotNetCore.WinFormsApp/FrmBlogList.cs
@@ -44,6 +44,8 @@ namespace TYZTDotNetCore.WinFormsApp
             //int rowIndex =e.RowIndex;
             if (e.RowIndex == -1) return;
 
+            #region If Case
+
             var blogId = Convert.ToInt32(dgvData.Rows[e.RowIndex].Cells["colId"].Value);
 
             if (e.ColumnIndex == (int)EnumFormControlType.Edit)
@@ -60,6 +62,35 @@ namespace TYZTDotNetCore.WinFormsApp
 
                 DeleteBlog(blogId);
             }
+
+            #endregion
+
+            #region Switch Case
+
+            //int index = e.ColumnIndex;
+            //EnumFormControlType enumFormControlType = (EnumFormControlType)index;
+            //switch (enumFormControlType)
+            //{
+            //    case EnumFormControlType.Edit:
+            //        FrmBLog frm = new FrmBLog(blogId);
+            //        frm.ShowDialog();
+
+            //        BlogList();
+            //        break;
+            //    case EnumFormControlType.Delete:
+            //        var dialogResult = MessageBox.Show("Are you sure want to delete?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            //        if (dialogResult != DialogResult.Yes) return;
+
+            //        DeleteBlog(blogId);
+            //        break;
+            //    case EnumFormControlType.None:
+            //    default:
+            //        MessageBox.Show("Invalid Case.");
+            //        break;
+            //}
+
+            #endregion
+
             //EnumFormControlType enumFormControlType = EnumFormControlType.None;
             //switch(enumFormControlType)
             //{
