d1c6f58cfbfc1e853ba3a5b583e0a4dd23ba4d7f
htunyarzar80 <htunyarzar2001@gmail.com>
2024-05-31 14:41:38 +0630

    [Complete] WindowFormsApp SqlInjection

---
+++ TYZTDotNetCore.WindowFormsAppSqlInjection/ConnectionStrings.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Data.SqlClient;
+
+namespace TYZTDotNetCore.WindowFormsApSqlInjection
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "ZACK",
+            InitialCatalog = "DotNet",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true,
+        };
+
+    }
+}
---
+++ TYZTDotNetCore.WindowFormsAppSqlInjection/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TYZTDotNetCore.WindowFormsApSqlInjection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(121, 134);
+            txtEmail.Margin = new Padding(4);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(374, 32);
+            txtEmail.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(121, 174);
+            txtPassword.Margin = new Padding(4);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(374, 32);
+            txtPassword.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.MenuHighlight;
+            button1.Location = new Point(121, 229);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 36);
+            button1.TabIndex = 2;
+            button1.Text = "&Login";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(601, 562);
+            Controls.Add(button1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Button button1;
+    }
+}
