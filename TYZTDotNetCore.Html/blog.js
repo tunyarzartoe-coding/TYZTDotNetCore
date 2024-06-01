5aed6c232e2b0e5c24471f94bb04529a1e60aaea
htunyarzar80 <htunyarzar2001@gmail.com>
2024-06-01 20:56:47 +0630

    [Complete] JQuery Blog CRUD

--- TYZTDotNetCore.Html/blog.js
+++ TYZTDotNetCore.Html/blog.js
@@ -1,29 +1,48 @@
 const tblBlog = "blog";
+let blogId = null;
 
+getBlogTable();
 // readBlog();
 // createBlog();
 //updateBlog("541dbd99-a0a6-4a58-b9e2-723a13d1ea44","updated","updated","updated")
 //deleteBlog("541dbd99-a0a6-4a58-b9e2-723a13d1ea44")
 
 function readBlog() {
-  const blogs = localStorage.getItem(tblBlog);
-  console.log(blogs);
+  let lst = getBlogs();
+  console.log(lst);
 }
 
-function createBlog() {
-  const blogs = localStorage.getItem(tblBlog);
-  console.log(blogs);
+function editBlog(id) {
+  let lst = getBlogs();
 
-  let lst = [];
-  if (blogs !== null) {
-    lst = JSON.parse(blogs);
+  const items = lst.filter((x) => x.id === id);
+  console.log(items);
+
+  console.log(items.length);
+
+  if (items.length == 0) {
+    console.log("no data found.");
+    errorMessage("no data found.");
+    return;
   }
+
+  let item = items[0];
+
+  blogId = item.id;
+  $("#txtTitle").val(item.title);
+  $("#txtAuthor").val(item.author);
+  $("#txtContent").val(item.content);
+  $("#txtTitle").focus();
+}
+
+function createBlog(title, author, content) {
+  let lst = getBlogs();
 
   const requestModal = {
     id: uuidv4(),
-    title: "Test Title",
-    author: "Test Author",
-    content: "Test Content",
+    title: title,
+    author: author,
+    content: content,
   };
 
   lst.push(requestModal);
@@ -36,11 +55,7 @@ function createBlog() {
 }
 
 function updateBlog(id, title, author, content) {
-  const blogs = localStorage.getItem(tblBlog);
-  let lst = [];
-  if (blogs !== null) {
-    lst = JSON.parse(blogs);
-  }
+  let lst = getBlogs();
 
   const items = lst.filter((x) => x.id === id);
   console.log("items ====>", items);
@@ -67,25 +82,24 @@ function updateBlog(id, title, author, content) {
 }
 
 function deleteBlog(id) {
-  const blogs = localStorage.getItem(tblBlog);
-  let lst = [];
-  if (blogs !== null) {
-    lst = JSON.parse(blogs);
-  }
+  let result = confirm("are you sure want to delete?");
+  if (!result) return;
+
+  let lst = getBlogs();
 
   const items = lst.filter((x) => x.id === id);
-  console.log("items ====>", items);
-  console.log(items.length);
-
   if (items.length == 0) {
     console.log("no data found.");
-    errorMessage("no data found.");
     return;
   }
 
   lst = lst.filter((x) => x.id !== id);
   const jsonBlog = JSON.stringify(lst);
   localStorage.setItem(tblBlog, jsonBlog);
+
+  successMessage("Deleting Successful.");
+
+  getBlogTable();
 }
 
 function uuidv4() {
@@ -95,4 +109,71 @@ function uuidv4() {
       (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
     ).toString(16)
   );
+}
+
+function getBlogs() {
+  const blogs = localStorage.getItem(tblBlog);
+  let lst = [];
+  if (blogs !== null) {
+    lst = JSON.parse(blogs);
+  }
+
+  return lst;
+}
+
+$("#btnSave").click(function () {
+  const title = $("#txtTitle").val();
+  const author = $("#txtAuthor").val();
+  const content = $("#txtContent").val();
+
+  if (blogId === null) {
+    createBlog(title, author, content);
+  } else {
+    updateBlog(blogId, title, author, content);
+    blogId = null;
+  }
+
+  getBlogTable();
+});
+
+function successMessage(message) {
+  alert(message);
+}
+
+function errorMessage(message) {
+  alert(message);
+}
+
+function clearControls() {
+  $("#txtTitle").val("");
+  $("#txtAuthor").val("");
+  $("#txtContent").val("");
+  $("#txtTitle").focus();
+}
+
+function getBlogTable() {
+  const lst = getBlogs();
+  let count = 0;
+  let htmlRows = "";
+  lst.forEach((item) => {
+    const htmlRow = `
+        <tr>
+            <td>
+                <button type="button" class="btn btn-warning" onclick="editBlog('${
+                  item.id
+                }')">Edit</button>
+                <button type="button" class="btn btn-danger" onclick="deleteBlog('${
+                  item.id
+                }')">Delete</button>
+            </td>
+            <td>${++count}</td>
+            <td>${item.title}</td>
+            <td>${item.author}</td>
+            <td>${item.content}</td>
+        </tr>
+        `;
+    htmlRows += htmlRow;
+  });
+
+  $("#tbody").html(htmlRows);
 }
