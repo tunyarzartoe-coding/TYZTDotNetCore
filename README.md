4c4e0e5cf1620fdc390ec85d025d4d8ab07d5fe1
htunyarzar80 <htunyarzar2001@gmail.com>
2024-06-19 14:11:44 +0630

    [Complete] MvcApp GetBlogs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
  Scaffold-DbContext "Server=ZACK;Database=DotNet;User ID=sa;Password=sa@123;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context AppDbContext -Tables Tbl_Blog
+
+ MVC = Model View Controller
+
+ Controller => Processing => View(Model == Dto) => UI(cshtml)
---
+++ TYZTDotNetCore.MvcApp/Db/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TYZTDotNetCore.MvcApp.Models;
+
+namespace TYZTDotNetCore.MvcApp.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        //}
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
