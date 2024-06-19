6575a328da733bd4bc63d667aa60fd6baa09355f
htunyarzar80 <htunyarzar2001@gmail.com>
2024-06-19 10:43:27 +0630

    [Complete] MinimalApi CRUD

---
+++ TYZTDotNetCore.MinimalApi/Db/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TYZTDotNetCore.MinimalApi.Models;
+
+namespace TYZTDotNetCore.MinimalApi.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        //}
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ TYZTDotNetCore.MinimalApi/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using TYZTDotNetCore.MinimalApi.Db;
+using TYZTDotNetCore.MinimalApi.Features.Blog;
+using TYZTDotNetCore.MinimalApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//dbConnection
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+},
+ServiceLifetime.Transient,
+ServiceLifetime.Transient);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//app.MapGet("/", () => "Hello World");
+
+//BlogServices.AddBlogFeatures(app);
+
+app.MapBlogs();
+
+app.Run();
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast")
+//.WithOpenApi();
+
+
+
+//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
