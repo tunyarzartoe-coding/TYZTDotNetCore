4d80aafce4b073426731c8182022855b525a4fc8
htunyarzar80 <htunyarzar2001@gmail.com>
2024-06-20 10:36:24 +0630

    [Complete] MVC App Create Blog

--- TYZTDotNetCore.MvcApp/Controllers/BlogController.cs
+++ TYZTDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TYZTDotNetCore.MvcApp.Db;
+using TYZTDotNetCore.MvcApp.Models;
 
 namespace TYZTDotNetCore.MvcApp.Controllers
 {
@@ -17,6 +18,22 @@ namespace TYZTDotNetCore.MvcApp.Controllers
         {
             var lst = await _db.Blogs.ToListAsync();
             return View(lst);
+        }
+
+        [ActionName("Create")]
+        public IActionResult CreateBlog()
+        {
+            return View("CreateBlog");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> CreateBlog(BlogModel blog)
+        {
+            await _db.Blogs.AddAsync(blog);
+            var result = await _db.SaveChangesAsync();
+            //return View("CreateBlog");
+            return Redirect("/Blog");
         }
     }
 }
