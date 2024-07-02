f79208a556ce5a1515af6749c41acc232111ccf8
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-02 23:15:21 +0630

    [Complete] ApexChart - SimplePieChart

---
+++ TYZTDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TYZTDotNetCore.MvcChartApp.Models;
+
+namespace TYZTDotNetCore.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult SimplePieChart()
+        {
+            SimplePieChartModel model = new SimplePieChartModel();
+            model.Lables = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
+            model.Series = new List<int> { 44, 55, 13, 43, 22 };
+            return View(model);
+        }
+    }
+}
---
+++ TYZTDotNetCore.MvcChartApp/Models/SimplePieChartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TYZTDotNetCore.MvcChartApp.Models
+{
+    public class SimplePieChartModel
+    {
+        public List<int> Series { get; set; }
+        public List<string> Lables { get; set; }
+    }
+}
