18f150379b145767b9aab820d62aff160507a1b9
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-09 23:17:35 +0630

    [Complete] CanvasJs Chart - Line Chart

---
+++ TYZTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TYZTDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
