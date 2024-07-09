6f55e15cb82425cabbfd705e59e0dece3ac9f46a
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-09 16:05:52 +0630

    [Complete] ChartJs - Example Chart

---
+++ TYZTDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TYZTDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult ExampleChart()
+        {
+            return View();
+        }
+    }
+}
