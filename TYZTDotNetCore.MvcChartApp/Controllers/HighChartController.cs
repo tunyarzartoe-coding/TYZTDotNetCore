b79cd5ed2c8d499d06e34d2596010729de15db44
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-09 22:54:20 +0630

    [Complete] HighChart - Pie Chart

---
+++ TYZTDotNetCore.MvcChartApp/Controllers/HighChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TYZTDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
