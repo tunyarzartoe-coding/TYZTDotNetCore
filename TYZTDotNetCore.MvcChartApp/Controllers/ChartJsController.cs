80ab57424f6fd8abbe22dbc2c2fc126fe74c8c0e
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-09 22:14:30 +0630

    [Complete] ChartJs - LineStylingChart

--- TYZTDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
+++ TYZTDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TYZTDotNetCore.MvcChartApp.Models;
 
 namespace TYZTDotNetCore.MvcChartApp.Controllers
 {
@@ -11,6 +12,45 @@ namespace TYZTDotNetCore.MvcChartApp.Controllers
         public IActionResult InterpolationLineChart()
         {
             return View();
+        }
+        public IActionResult LineStylingChart()
+        {
+            var model = new LineStylingChartModel
+            {
+                Labels = new List<string> { "January", "February", "March", "April", "May", "June", "July" },
+                DataSets = new List<DataSets>
+                {
+                    new DataSets
+                    {
+                        Label = "Unfilled",
+                        Fill = false,
+                        BgColor = "rgb(54, 162, 235)",
+                        BorderColor = "rgb(54, 162, 235)",
+                        BorderDash = new List<int>(),
+                        Data = new List<int> { 65, -59, 80, 81, -56, 55, 40 }
+                    },
+                    new DataSets
+                    {
+                        Label = "Dashed",
+                        Fill = false,
+                        BgColor = "rgb(75, 192, 192)",
+                        BorderColor = "rgb(75, 192, 192)",
+                        BorderDash = new List<int> { 5, 5 },
+                        Data = new List<int> { -28, 48, -40, 19, 86, 27, 90 }
+                    },
+                    new DataSets
+                    {
+                        Label = "Filled",
+                        Fill = true,
+                        BgColor = "rgb(255, 99, 132)",
+                        BorderColor = "rgb(255, 99, 132)",
+                        BorderDash = new List<int>(),
+                        Data = new List<int> { 18, -48, 77, 9, 100, -27, 40 }
+                    }
+                }
+            };
+
+            return View(model);
         }
     }
 }
---
+++ TYZTDotNetCore.MvcChartApp/Models/LineStylingChartModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TYZTDotNetCore.MvcChartApp.Models
+{
+    public class LineStylingChartModel
+    {
+        public List<string> Labels { get; set; }
+        public List<DataSets> DataSets { get; set; }
+    }
+    public class DataSets
+    {
+        public string Label { get; set; }
+        public bool Fill { get; set; }
+        public string BgColor { get; set; }
+        public string BorderColor { get; set; }
+        public List<int> BorderDash { get; set; }
+        public List<int> Data {  get; set; }
+
+
+    }
+}
