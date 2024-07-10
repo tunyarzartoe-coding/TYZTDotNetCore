433fdc8b28ab231469f5b0d5176b4828a5b041a3
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-10 11:49:43 +0630

    [Complete] ASP.NET Core Text Logging

--- TYZTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
+++ TYZTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -4,8 +4,16 @@ namespace TYZTDotNetCore.MvcChartApp.Controllers
 {
     public class CanvasJsController : Controller
     {
+        private readonly ILogger<CanvasJsController> _logger;
+
+        public CanvasJsController(ILogger<CanvasJsController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult LineChart()
         {
+            _logger.LogInformation("Line Chart...");
             return View();
         }
         public IActionResult AnimatedChart()
