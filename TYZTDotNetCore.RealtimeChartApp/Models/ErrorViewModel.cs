8d1036bbad08e4801bc6b78c9169bdc5a9ba1cce
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-11 16:29:01 +0630

    [Complete] Realtime Chart App

---
+++ TYZTDotNetCore.RealtimeChartApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TYZTDotNetCore.RealtimeChartApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
