d7f04820f132528b82f559b2a7252758e6e48530
htunyarzar80 <htunyarzar2001@gmail.com>
2024-08-12 14:08:58 +0630

    [Complete] ASP.Net Core MVC UI Design

---
+++ TYZTDotNetCore.MvcDesignApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TYZTDotNetCore.MvcDesignApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
