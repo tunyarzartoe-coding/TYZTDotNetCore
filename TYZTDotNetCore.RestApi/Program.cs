6904930478ba7a9a77e20dce1f20a01a59e7f64b
htunyarzar80 <htunyarzar2001@gmail.com>
2024-08-22 13:52:32 +0630

    [complete] BlazorWasm Get Blog

--- TYZTDotNetCore.RestApi/Program.cs
+++ TYZTDotNetCore.RestApi/Program.cs
@@ -2,7 +2,23 @@ using Microsoft.EntityFrameworkCore;
 using TYZTDotNetCore.RestApi.Db;
 using TYZTDotNetCore.Shared;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:7193","http://localhost:5074")
+                                .WithMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                                .AllowAnyHeader();
+                      });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
 
 // Add services to the container.
 
@@ -38,6 +54,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
