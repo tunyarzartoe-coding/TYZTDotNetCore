029c3425109c70e2833694d7636deb424ae34b31
htunyarzar80 <htunyarzar2001@gmail.com>
2024-08-23 14:01:29 +0630

    [complete] Blazor Server -  edit blog

--- TYZTDotNetCore.BlazorServer/Program.cs
+++ TYZTDotNetCore.BlazorServer/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-});
+},ServiceLifetime.Transient,ServiceLifetime.Transient);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
